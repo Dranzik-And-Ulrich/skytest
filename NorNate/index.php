f330cccf2bdd4df7132ab0cf70a98ce1b498e151
Metrix187 <33392918+Metrix187@users.noreply.github.com>
2019-01-09 18:58:23 -0500

    Making A Launcher Website
    Added Bing instead of Google just cuz its better

---
+++ NorNate/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>Nornate Launcher</h1>
+<p>Basic Website Launcher</p>
+<p>Just Click To Go There</p>
+ <a href="https://www.bing.com" target="_blank" >Bing</a>
+
+</body>
+</html>
